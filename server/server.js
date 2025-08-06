fdb88df19adc22672cf43d33a795bb9abad16f0e
Rawan Sharif <dotfive25@gmail.com>
2025-08-06 10:33:21 +0300

    latest 8 commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
